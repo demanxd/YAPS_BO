10c82f4ccb15458b7240ba0d49aa4eee57ab6092
ShXD <sh@test.com>
2021-04-16 10:50:30 +0300

    Some improvements for loading levels

--- Project_Rise/godot/BlockOut/aux_tscn/NPC.gd
+++ Project_Rise/godot/BlockOut/aux_tscn/NPC.gd
@@ -2,6 +2,8 @@ class_name NPC
 extends Walker
 
 
+export var root : = NodePath()
+
 
 func _extends_phys_process():
 	if debug:
@@ -10,12 +12,14 @@ func _extends_phys_process():
 func _extends_left_trig_process():
 	if debug:
 		print(self.name + ": left_trigger pricessing (<-)")
+		end_lvl()
 
 func _extends_right_trig_process():
 	if debug:
 		print(self.name + ": right_trigger processing (->)")
+		end_lvl()
 
 
 func end_lvl():
-	
+	get_tree().change_scene("res://tscn_folders/GUI.tscn")
 	pass
--- Project_Rise/godot/BlockOut/tscn_folders/GUI.gd
+++ Project_Rise/godot/BlockOut/tscn_folders/GUI.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 onready var root
+onready var lineEdit = $LineEdit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +12,10 @@ func _ready():
 
 func _on_Button_button_down():
 #	root.start_game()
-	get_tree().change_scene("res://tscn_folders/Tavern.tscn")
-	pass # Replace with function body.
+	var num = lineEdit.text.to_int()
+	if num == 0:
+		get_tree().change_scene("res://tscn_folders/Tavern.tscn")
+	if num == 1:
+		get_tree().change_scene("res://aux_tscn/Empty_scene.tscn")
+	else:
+		print("Wrong number!")
